3bebadafddc1f7c33bcd22f800d5525993ead58f
Kevin Andres González Dinas <147012730+Andrewgo12@users.noreply.github.com>
2024-11-09 18:51:52 -0500

    Update Productos.js

--- Proyecto Final Fundamentos Web/js/Productos.js
+++ Proyecto Final Fundamentos Web/js/Productos.js
@@ -10,7 +10,7 @@ const productos =
             "descripcion": "Detergente concentrado para ropa.",
             "precio": 15000,
             "imagen": "https://www.kipclin.com/images/virtuemart/product/KIP-PQP-DETER00999(E).jpg",
-            "cantidad": 29
+            "cantidad": 29,
         },
         {
             "id": 1002,
@@ -18,7 +18,7 @@ const productos =
             "descripcion": "Ideal para superficies duras.",
             "precio": 10000,
             "imagen": "https://mercaldas.vtexassets.com/arquivos/ids/1325813/Limpiador-DERSA-multiusos-desinfectante-x4000-ml_120391.jpg?v=638508604253470000s",
-            "cantidad": 50
+            "cantidad": 50,
         },
         {
             "id": 1003,
@@ -26,7 +26,7 @@ const productos =
             "descripcion": "Elimina el 99.9% de gérmenes.",
             "precio": 20000,
             "imagen": "https://olimpica.vtexassets.com/arquivos/ids/1327291/7793253471705_2.jpg?v=638452530742270000",
-            "cantidad": 40
+            "cantidad": 40,
         },
         {
             "id": 1004,
@@ -34,7 +34,7 @@ const productos =
             "descripcion": "Jabón biodegradable para manos.",
             "precio": 5000,
             "imagen": "https://www.cerowasteshop.com/cdn/shop/products/Jabon-Para-Manos-Natural-Ecologico-Biodegradable-500-ml.jpg?v=1685639410&width=1445",
-            "cantidad": 50
+            "cantidad": 50,
         },
         {
             "id": 1005,
@@ -42,7 +42,7 @@ const productos =
             "descripcion": "Absorbentes y resistentes.",
             "precio": 12000,
             "imagen": "https://www.catalogoespacial.com/16656-large_default/toallas-de-papel.jpg",
-            "cantidad": 150
+            "cantidad": 150,
         },
         {
             "id": 1006,
@@ -50,7 +50,7 @@ const productos =
             "descripcion": "Escoba tradicional para barrer.",
             "precio": 25000,
             "imagen": "https://www.loencuentras.com.co/1916-large_default/escoba-madera.jpg",
-            "cantidad": 55
+            "cantidad": 55,
         },
         {
             "id": 1007,
@@ -58,7 +58,7 @@ const productos =
             "descripcion": "Recogedor plástico de alta resistencia.",
             "precio": 10000,
             "imagen": "https://detalshop.com/cdn/shop/products/022366.jpg?v=1588392867",
-            "cantidad": 296
+            "cantidad": 296,
         },
         {
             "id": 1008,
@@ -66,7 +66,7 @@ const productos =
             "descripcion": "Perfecto para pisos duros.",
             "precio": 30000,
             "imagen": "https://canelahogar.com.co/wp-content/uploads/2021/08/7016005-TraperoMicroFibraAzul.jpg",
-            "cantidad": 40
+            "cantidad": 40,
         },
         {
             "id": 1009,
@@ -74,7 +74,7 @@ const productos =
             "descripcion": "Guantes desechables para tareas domésticas.",
             "precio": 5000,
             "imagen": "https://http2.mlstatic.com/D_NQ_NP_661197-MCO47379769348_092021-O.webp",
-            "cantidad": 68
+            "cantidad": 68,
         },
         {
             "id": 1010,
@@ -82,7 +82,7 @@ const productos =
             "descripcion": "Esponjas suaves y duraderas.",
             "precio": 7000,
             "imagen": "https://dojiw2m9tvv09.cloudfront.net/12840/product/M_esponja-lisa3798.png?11&time=1728407800",
-            "cantidad": 297
+            "cantidad": 297,
         },
         {
             "id": 1011,
@@ -90,7 +90,7 @@ const productos =
             "descripcion": "Bolsas resistentes para desechos.",
             "precio": 8000,
             "imagen": "https://centraldesuministrosgs.com/wp-content/uploads/2023/12/Bolsa-Plastica-de-65X80cm-paqx30und-color-negrox18g-central-de-suministrosgs1.webp",
-            "cantidad": 2000
+            "cantidad": 2000,
         },
         {
             "id": 1012,
@@ -98,7 +98,7 @@ const productos =
             "descripcion": "Para ventanas brillantes y sin rayas.",
             " precio": 10000,
             "imagen": "https://http2.mlstatic.com/D_NQ_NP_706057-MCO45418130666_042021-O.webp",
-            "cantidad": 330
+            "cantidad": 330,
         },
         {
             "id": 1013,
@@ -106,7 +106,7 @@ const productos =
             "descripcion": "Fragancia fresca para el hogar.",
             "precio": 7000,
             "imagen": "https://exitocol.vtexassets.com/arquivos/ids/24447946/Ambientador-Spray-Lavanda-HOMEBRIGHT-MARCA-EXCLUSIVA-30177-3393959_a.jpg?v=638609471481300000",
-            "cantidad": 294
+            "cantidad": 294,
         },
         {
             "id": 1014,
@@ -114,7 +114,7 @@ const productos =
             "descripcion": "Fregona de algodón para limpieza profunda.",
             "precio": 15000,
             "imagen": "https://cuatrogasaprofesional.com/wp-content/uploads/2023/07/BF-F0160-VE-FRTI_Pack_sd_96ppp.jpg",
-            "cantidad": 223
+            "cantidad": 223,
         },
         {
             "id": 1015,
@@ -122,7 +122,7 @@ const productos =
             "descripcion": "Elimina manchas y desinfecta.",
             "precio": 12000,
             "imagen": "https://cdn1.totalcommerce.cloud/homesentry/product-zoom/es/limpiador-de-banos-mr-musculo_naranja-con-blanco-1.webp",
-            "cantidad": 293
+            "cantidad": 293,
         },
         {
             "id": 1016,
@@ -130,7 +130,7 @@ const productos =
             "descripcion": "Fórmula concentrada para todo tipo de pisos.",
             "precio": 18000,
             "imagen": "https://vaquitaexpress.com.co/media/catalog/product/cache/e89ece728e3939ca368b457071d3c0be/7/7/7707815552078_29.jpg",
-            "cantidad": 298
+            "cantidad": 298,
         },
         {
             "id": 1017,
@@ -138,7 +138,7 @@ const productos =
             " descripcion": "Ideales para secar y pulir.",
             "precio": 6000,
             "imagen": "https://http2.mlstatic.com/D_NQ_NP_797432-MLA44041279363_112020-O.webp",
-            "cantidad": 2953
+            "cantidad": 2953,
         },
         {
             "id": 1018,
@@ -146,7 +146,7 @@ const productos =
             "descripcion": "Elimina grasa y suciedad difícil.",
             "precio": 14000,
             "imagen": "https://dcdn.mitiendanube.com/stores/001/696/864/products/desengrasante-liquido-por-500cc-afc70fe80def5764ae17072485839184-1024-1024.png",
-            "cantidad": 223
+            "cantidad": 223,
         },
         {
             "id": 1019,
@@ -154,7 +154,7 @@ const productos =
             "descripcion": "Elimina manchas y olores.",
             "precio": 25000,
             "imagen": "https://exitocol.vtexassets.com/arquivos/ids/24442331/Limpiador-De-Alfombra-X-650-ml-855621_a.jpg?v=638609322891800000",
-            "cantidad": 223
+            "cantidad": 223,
         },
         {
             "id": 1020,
@@ -162,7 +162,7 @@ const productos =
             "descripcion": "Ideal para limpiar rincones difíciles.",
             "precio": 8000,
             "imagen": "https://http2.mlstatic.com/D_NQ_NP_715235-MLU71568057782_092023-O.webp",
-            "cantidad": 2234
+            "cantidad": 2234,
         },
         {
             "id": 1021,
@@ -170,7 +170,7 @@ const productos =
             "descripcion": "Deja un acabado brillante en los muebles.",
             "precio": 12000,
             "imagen": "https://dafesa.com/wp-content/uploads/2019/11/Limpiador-muebles-06.jpg",
-            "cantidad": 2224
+            "cantidad": 2224,
         },
         {
             "id": 1022,
@@ -178,7 +178,7 @@ const productos =
             "descripcion": "Para tratar manchas en ropa.",
             "precio": 10000,
             "imagen": "https://m.media-amazon.com/images/I/61levC7cZmL._SL1280_.jpg",
-            "cantidad": 223
+            "cantidad": 223,
         },
         {
             "id": 1023,
@@ -186,7 +186,7 @@ const productos =
             "descripcion": "Limpieza efectiva y amigable con el medio ambiente.",
             "precio": 15000,
             "imagen": "https://sgfm.elcorteingles.es/SGFM/dctm/MEDIA03/202210/31/00128013200945____9__325x325.jpg",
-            "cantidad": 224
+            "cantidad": 224,
         },
         {
             "id": 1024,
@@ -194,7 +194,7 @@ const productos =
             "descripcion": "Suave y resistente.",
             "precio": 5000,
             "imagen": "https://lirp.cdn-website.com/1a6c14d7/dms3rep/multi/opt/image007-640w.jpg",
-            "cantidad": 225
+            "cantidad": 225,
         },
         {
             "id": 1025,
@@ -202,7 +202,7 @@ const productos =
             "descripcion": "Neutraliza olores desagradables.",
             "precio": 10000,
             "imagen": "https://images.ctfassets.net/tcoigcjw85h2/48LDdMs6CUi1Dzu1CFjWaX/d8c24b11087cd05efbdabb90110730d2/eliminador-olores-bano-aires-lavanda.png",
-            "cantidad": 223465
+            "cantidad": 223465,
         },
         {
             "id": 1026,
@@ -210,7 +210,7 @@ const productos =
             "descripcion": "Protege tus manos mientras cocinas.",
             "precio": 8000,
             "imagen": "https://estra.vtexassets.com/arquivos/ids/182775/4-1049699_Guante-Para-Cocina-De-Silicona-.jpg?v=638405948876200000",
-            "cantidad": 225
+            "cantidad": 225,
         },
         {
             "id": 1027,
@@ -218,7 +218,7 @@ const productos =
             " descripcion": "Cápsulas para una dosificación precisa.",
             "precio": 18000,
             "imagen": "https://m.media-amazon.com/images/I/61U9J0JUcSL._AC_UF1000,1000_QL80_.jpg",
-            "cantidad": 225
+            "cantidad": 225,
         },
         {
             "id": 1028,
@@ -226,7 +226,7 @@ const productos =
             "descripcion": "Para una limpieza rápida y efectiva.",
             "precio": 12000,
             "imagen": "https://cdn1.totalcommerce.cloud/homesentry/product-zoom/es/limpiador-vidrios-antiempanante-citrus-azul-1.webp",
-            "cantidad": 227
+            "cantidad": 227,
         },
         {
             "id": 1029,
@@ -234,7 +234,7 @@ const productos =
             "descripcion": "Elimina grasa y suciedad del horno.",
             "precio": 15000,
             "imagen": "https://maxitenjo.com.co/cdn/shop/products/limpiahornos-magistral-x-180-gr-1_800x.jpg?v=1597944051",
-            "cantidad": 227
+            "cantidad": 227,
         },
         {
             "id": 1030,
@@ -242,7 +242,7 @@ const productos =
             "descripcion": "Cubo para el trapeador.",
             "precio": 20000,
             "imagen": "https://m.media-amazon.com/images/I/514t+vjLjhL._AC_UF350,350_QL80_.jpg",
-            "cantidad": 227
+            "cantidad": 227,
         },
         {
             "id": 1031,
@@ -250,7 +250,7 @@ const productos =
             "descripcion": "Fórmula para limpiar la cocina a fondo.",
             "precio": 22000,
             "imagen": "https://m.media-amazon.com/images/I/81m7e1N8pzL._AC_UF894,1000_QL80_.jpg",
-            "cantidad": 226
+            "cantidad": 226,
         },
         {
             "id": 1032,
@@ -258,7 +258,7 @@ const productos =
             "descripcion": "Para desinfectar todas las superficies del hogar.",
             "precio": 20000,
             "imagen": "https://www.cif.com.ar/images/h0nadbhvm6m4/4rugFP9MsKKtwD5URTFc2t/45cd0c51f72b52b0f799a1734d64b21c/Q2lmX0dhdGlsbG9fRGVzaW5mZWN0YW50ZS5wbmc/1100w-1100h/cif-limpiador-l%C3%ADquido-desinfectante-de-superficies.jpg",
-            "cantidad": 2234
+            "cantidad": 2234,
         },
         {
             "id": 1033,
@@ -266,7 +266,7 @@ const productos =
             "descripcion": "Ideales para limpieza y desinfección.",
             "precio": 5000,
             "imagen": "https://maken.com.co/wp-content/uploads/2020/08/toallas-para-manos-maken-copia.jpg",
-            "cantidad": 226
+            "cantidad": 226,
         },
         {
             "id": 1035,
@@ -274,7 +274,7 @@ const productos =
             "descripcion": "Para limpiar y proteger pisos de madera.",
             "precio": 18000,
             "imagen": "https://http2.mlstatic.com/D_NQ_NP_751831-MLU76259015857_052024-O.webp",
-            "cantidad": 223
+            "cantidad": 223,
         },
         {
             "id": 1036,
@@ -282,7 +282,7 @@ const productos =
             "descripcion": "Elimina manchas en muebles tapizados.",
             "precio": 22000,
             "imagen": "https://www.velky.store/wp-content/uploads/2020/05/upholstery-cleaner-32oz-1518x1518.jpg",
-            "cantidad": 2234
+            "cantidad": 2234,
         },
         {
             "id": 1037,
@@ -290,7 +290,7 @@ const productos =
             "descripcion": "Gel desinfectante para manos.",
             "precio": 6000,
             "imagen": "https://holandinacolombia.com/wp-content/uploads/2024/01/geldin-a-gel-antibacterial-antiseptico-holandina-500ml.jpg",
-            "cantidad": 287
+            "cantidad": 287,
         },
         {
             "id": 1038,
@@ -298,7 +298,7 @@ const productos =
             "descripcion": "Para una limpieza profunda de parrillas.",
             "precio": 15000,
             "imagen": "https://charbroil.com.co/wp-content/uploads/2019/06/Limpiador-de-parrilla-8416558R06-2.jpg",
-            "cantidad": 296
+            "cantidad": 296,
         },
         {
             "id": 1039,
@@ -306,7 +306,7 @@ const productos =
             "descripcion": "Fórmula eficaz para limpiar cerámica.",
             "precio": 10000,
             "imagen": "https://olimpica.vtexassets.com/arquivos/ids/1319642/7702155028603.jpg?v=638446432011470000",
-            "cantidad": 267
+            "cantidad": 267,
         },
         {
             "id": 1040,
@@ -314,7 +314,7 @@ const productos =
             "descripcion": "Limpiador a vapor para eliminar gérmenes.",
             "precio": 100000,
             "imagen": "https://exitocol.vtexassets.com/arquivos/ids/18843320/maquina-a-vapor-limpieza-para-casa-5-en-1.jpg?v=638217469392570000",
-            "cantidad": 295
+            "cantidad": 295,
         },
         {
             "id": 1041,
@@ -322,7 +322,7 @@ const productos =
             "descripcion": "Neutraliza olores causados por mascotas.",
             "precio": 15000,
             "imagen": "https://www.faggidistribuciones.com.co/wp-content/uploads/2018/07/eliminador-de-olores-mascotas-brizze-320-ml.jpg",
-            "cantidad": 223
+            "cantidad": 223,
         },
         {
             "id": 1042,
@@ -330,7 +330,7 @@ const productos =
             "descripcion": "Para abrillantar pisos y muebles.",
             "precio": 20000,
             "imagen": "https://pulitocarwash.co/wp-content/uploads/2020/07/IMG_0047.jpg",
-            "cantidad": 2943
+            "cantidad": 2943,
         },
         {
             "id": 1043,
@@ -338,7 +338,7 @@ const productos =
             "descripcion": "Para la separación de residuos reciclables.",
             "precio": 8000,
             "imagen": "https://i0.wp.com/stopbasura.com/wp-content/uploads/2023/01/foto2.jpg?fit=800%2C800&ssl=1",
-            "cantidad": 223
+            "cantidad": 223,
         },
         {
             "id": 1044,
@@ -354,7 +354,7 @@ const productos =
             "descripcion": "Raspador para eliminar suciedad resistente.",
             "precio": 5000,
             "imagen": "https://img.kwcdn.com/product/open/2023-12-28/1703747008180-093f36faf6814c2fbf4eb9cc6f383fd1-goods.jpeg?imageMogr2/auto-orient%7CimageView2/2/w/800/q/70/format/webp",
-            "cantidad": 294
+            "cantidad": 294,
         },
         {
             "id": 1046,
@@ -362,7 +362,7 @@ const productos =
             "descripcion": "Espuma para limpiar alfombras.",
             "precio": 15000,
             "imagen": "https://chedrauimx.vtexassets.com/arquivos/ids/36627744/7501066200043_02.jpg?v=638630624760430000",
-            "cantidad": 296
+            "cantidad": 296,
         },
         {
             "id": 1047,
@@ -370,7 +370,7 @@ const productos =
             "descripcion": "Para abrillantar y proteger pisos.",
             "precio": 12000,
             "imagen": "https://olimpica.vtexassets.com/arquivos/ids/611026/7701008227446.jpg?v=637626512766570000",
-            "cantidad": 294
+            "cantidad": 294,
         },
         {
             "id": 1048,
@@ -378,7 +378,7 @@ const productos =
             "descripcion": "Absorbentes y prácticas.",
             "precio": 7000,
             "imagen": "https://olimpica.vtexassets.com/arquivos/ids/1047689/image-945595bdf6b545988676506a6a8a5c84.jpg?v=638119997170930000",
-            "cantidad": 242
+            "cantidad": 242,
         },
         {
             "id": 1049,
@@ -386,10 +386,8 @@ const productos =
             "descripcion": "Para la comodidad de los bebés.",
             "precio": 40000,
             "imagen": "https://www.ipetplace.com.co/wp-content/uploads/2023/10/PanalesDesechablesxl.jpg",
-            "cantidad": 2923
+            "cantidad": 2923,
         }
-
-
     ];
 
 /*=========== Comprobación y Carga Inicial ===========*/
